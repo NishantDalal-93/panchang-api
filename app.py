f3e86635912856373fe26ffba0eda89b697ee455
NishantDalal-93 <nishantdalal93@gmail.com>
2025-03-26 10:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,58 @@
 from flask import Flask, request, jsonify
-import swisseph as swe
-import datetime
+from skyfield.api import load
+from datetime import datetime
 import pytz
 
 app = Flask(__name__)
-swe.set_ephe_path('/usr/share/ephe')  # Path to Swiss Ephemeris
 
-@app.route('/panchang', methods=['GET'])
+def calculate_tithi(moon_lon, sun_lon):
+    angle = (moon_lon - sun_lon) % 360
+    tithi = int(angle // 12) + 1
+    return tithi
+
+def calculate_nakshatra(moon_lon):
+    nakshatra = int((moon_lon % 360) // (360 / 27)) + 1
+    return nakshatra
+
+@app.route('/panchang')
 def get_panchang():
-    date = request.args.get('date')  # Format: YYYY-MM-DD
-    lat = float(request.args.get('lat'))
-    lng = float(request.args.get('lng'))
-    tz_name = request.args.get('timezone', 'Asia/Kolkata')
-
     try:
-        # Convert date to Julian Day
-        dt = datetime.datetime.strptime(date, "%Y-%m-%d")
-        tz = pytz.timezone(tz_name)
-        local_dt = tz.localize(dt)
+        date_str = request.args.get('date')
+        lat = float(request.args.get('lat'))
+        lng = float(request.args.get('lng'))
+        timezone = request.args.get('timezone', 'Asia/Kolkata')
+
+        eph = load('de421.bsp')
+        ts = load.timescale()
+        tz = pytz.timezone(timezone)
+        local_dt = tz.localize(datetime.strptime(date_str, '%Y-%m-%d'))
         utc_dt = local_dt.astimezone(pytz.utc)
-        jd = swe.julday(utc_dt.year, utc_dt.month, utc_dt.day, utc_dt.hour + utc_dt.minute / 60)
+        t = ts.utc(utc_dt.year, utc_dt.month, utc_dt.day)
 
-        # Get sun and moon longitude
-        sun_long = swe.calc_ut(jd, swe.SUN)[0][0]
-        moon_long = swe.calc_ut(jd, swe.MOON)[0][0]
+        sun = eph['sun']
+        moon = eph['moon']
+        earth = eph['earth']
 
-        # Tithi and Nakshatra
-        angular_diff = (moon_long - sun_long) % 360
-        tithi_num = int(angular_diff / 12) + 1
-        nakshatra_num = int((moon_long % 360) / (360 / 27)) + 1
+        astrometric_sun = earth.at(t).observe(sun).apparent()
+        astrometric_moon = earth.at(t).observe(moon).apparent()
+
+        sun_lon, _, _ = astrometric_sun.ecliptic_latlon()
+        moon_lon, _, _ = astrometric_moon.ecliptic_latlon()
+
+        tithi = calculate_tithi(moon_lon.degrees, sun_lon.degrees)
+        nakshatra = calculate_nakshatra(moon_lon.degrees)
+
+        sunrise = "06:00 AM"
+        sunset = "06:30 PM"
 
         return jsonify({
-            "date": date,
+            "date": date_str,
             "latitude": lat,
             "longitude": lng,
-            "tithi_number": tithi_num,
-            "nakshatra_number": nakshatra_num,
-            "sunrise": "06:00 AM",
-            "sunset": "06:30 PM"
+            "tithi_number": tithi,
+            "nakshatra_number": nakshatra,
+            "sunrise": sunrise,
+            "sunset": sunset
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
